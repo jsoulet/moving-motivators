5512089e7fcf184fcaf3a664459e01108ef99b1a
Johan Soulet <johan.soulet@gmail.com>
2020-02-13 02:05:37 +0100

    :lipstick: adapt main container to window height

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import CardList from './CardList';
 
 const App = () => {
   return (
-    <div className="bg-gray-200 font-sans leading-normal tracking-normal h-screen">
+    <div className="bg-gray-200 font-sans leading-normal tracking-normal min-h-screen max-h-full">
       <CardList />
     </div>
   );
